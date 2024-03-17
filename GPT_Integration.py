6708ad72d668e0dde05f4cb4973cbfd182339d9a
esinocchi <163606662+esinocchi@users.noreply.github.com>
2024-03-17 02:58:09 -0400

    Update GPT_Integration.py

--- GPT_Integration.py
+++ GPT_Integration.py
@@ -5,63 +5,60 @@ import Flowchart
 import json
 
 load_dotenv()
-My_API_KEY = os.getenv('MY_API_KEY')
-client = OpenAI(api_key = My_API_KEY)
+OPENAI_API_KEY = os.getenv('MY_API_KEY')
+client = OpenAI(api_key = OPENAI_API_KEY)
 
 
 semester = '0'
-taken_courses = ''
+taken_courses = 'CMPSC 131'
 preferences = 'None'
 courses_str = json.dumps(Flowchart.cmpsc_courses, indent=2)
-gen_ed_str = cmpsc_courses_str = json.dumps(Flowchart.general_education, indent=2)
+gen_ed_str = json.dumps(Flowchart.general_education, indent=2)
 cmpsc_electives_str = json.dumps(Flowchart.CMPSC_Electives, indent=2)
 four_credit_str = json.dumps(Flowchart.four_credit, indent=4)
-priorty_str = json.dumps(Flowchart.priorty, indent=4)
+priority_str = json.dumps(Flowchart.priority, indent=4)
 
-introduction = "You are a Penn State Engineering Advisor, specifically for Computer Science. Your goal is to create an academic schedule based off the given content."
-explain_main_courses = f"Here is a dictionary with all the keys as required courses to take as a Computer Science Major. These courses NEED to be in the schedule. Also, CMPSC 111 needs to be taken in semester 1 or 2 since it is a first-year-seminar class.\
-           You must ensure that the student takes another CMPSC course in the same semester as CMPSC 111, additionally, ENGL 015 should be prioritized to be taken in semester 1 or 2.\
-           Prioritize taking the courses in {priorty_str} because these are the core classes you need to get started in the Computer Science track \
-           An element can be a equivalent course you can take if there is a dash (-) in front of it. \
-           If the characters -c are attached to the end of a course, this signifies that the course is a concurrent requirement for the key course.\
-           Every other element is a pre-requisite (prereq) course. Pre-requisite courses MUST be taken before the course that it is a pre-requisite for. Every course (key) within this dictionary should be considered a major course."
-explain_general_education = "Here is a dictionary with all of the general education credits. \
-           Each key can be treated as a course. \
-           Each key's value refers to the total credits that are needed for that specific course.\
-           General education credit courses can be displayed as their key values (type of general education credit - not actual specific course) when put into a schedule.\
-           Within a single semester in the schedule, you must include 1-2 general education courses. \
-           When adding the general education courses to the schedule, add each course 3 credits as each course."
-explain_CMPSC_electives = f"Within the dictionary of all the general education credits, there is a key that is labeled \"CMPSC Electives\"\
-           Here is an additional dictionary named CMPSC_Electives. Based on the user's preferences being: {preferences}, choose 6 credits worth of courses.  \
-           The courses in the CMPSC_Electives will fulfull the CMPSC Electives general education requirement"
-explain_credit_values = "Here is a list of all the courses with a credit value of 4.\
-           CMPSC 111 is 1 credit. If a course is not within this list and is not CMPSC 111, it can be assumed that the course is worth 3 credits."
-explain_courses_taken = "Here is a list of all of the credits that the student has already taken. \
-            Add all of these credits to the total credits needed for the student's degree. These courses have already been taken so do not include them into the schedule."
-explain_major_credits = "This number is the total required major credits, that is, the total number of credits from courses that are not considered general education credits needed for a student to obtain their Computer Science degree."
-create = f"Given that the student has already taken {semester} semesters, create a schedule for the student's remaining semesters that satisfies the major credits that are still needed and the general education credits that are still needed.\
-           Attempt to satisfy the student's CMPSC_electives courses preferences but do not prioritize those preferences.\
-           Keep the total credits that a student takes within a semester between 15 and 19. Do not display credit values at all. Format the schedule as a dictionary "
+prompt_one = f'You are a Penn State Engineering Advisor, specifically for Computer Science. Your goal is to create an academic schedule that meets graduation requirenets based off the given content. \
+Attached is a dictonary: {courses_str} , with the keys as required courses needed to graduate. Lets call the key, key course. The values is a dictionary. \
+In that dictionary, there are concurrent courses which can be taken with the key course, then pre-requisite courses that must be taken BEFORE the key course. \
+Another dictionary: {priority_str} , is a list of courses that you are going to prioritize to take as soon as possible.'
+
+prompt_two = f'In addition, there is a dictionary: {gen_ed_str} , with all of the general education credits. Each key is treated as a course and the value refers to the total credits needed to fulfill the general education requirement. Add enough general educuation courses to fulfill its specific credit requirement. \
+In the general education dictionary, there is a key named CMPSC Electives. This is a special case. There is another dictionary named preferences: {preferences}. Based off the preferences, choose courses in the CMPSPC Electives dictionary: {cmpsc_electives_str}. Take enough credits of the classes provided to fulfill the cmpsc electives general education requirement.\
+NOTE: The CMPSC electives are DIFFERENT than the department (engineering) electives. For the credit values, here is a list: {four_credit_str}, with all courses that have 4 credits. CMPSC 111 has 1 credit. Assume all other courses are 3 credits. \
+Given that the student has already taken: {semester}, in the schedule create the remaining semesters so the total semesters adds up to 8. For example, if someone has already taken 3 semesters, create 5.'
+
+prompt_three = f'This string: {taken_courses}, has all the courses already taken. Do not add these courses in the string to the schedule. These also fulfill the prequisite requirements. For example, if you have already taken MATH 140, you can take MATH 141. \
+If the user has taken 0 semesters, these are the UNIQUE requirements for the first two semesters: must take CMPSC 111, ENG 015, CMPSC 131 and most of the other classes in priority dictionary mentioned before \
+These are the requirements for each semesters, including the first two: 15-19 credits, 1-2 general education courses \
+These are the requirements for the entire schedule: 8 semesters max, 45 general education credits, 82 major credits\
+Output the schedule in the form of a dictionary. Do not include credits in the schedule. Include type of general education credit. A sample output looks like: \
+    Semester 1: \
+        CMPSC 131 \
+        MATH 140 \
+        ENGL 015 \
+        GN General Education \
+    ... \
+    Semester 7: \
+        CMPSC 450 \
+        CMPSC 483W or CMPSC 431W \
+        Department (Engineering) Elective \
+    In addition, \'Department (Engineering) Elective\' is added, treat it as a 3 credit course. Therefore you will need to add it three times. Implement this into a dictionary.'
 
 
 chat_completion = client.chat.completions.create(
     messages=[
-        {"role": "system", "content": ""},
-        {"role": "system", "content": explain_main_courses},  # Explain main courses
-        {"role": "system", "content": Flowchart.cmpsc_courses},
-        {"role": "system", "content": explain_general_education}, # Explain gen eds
-        {"role": "system", "content": Flowchart.general_education},
-        {"role": "system", "content": explain_CMPSC_electives}, # Explain CMPSC Electives (preferences)
-        {"role": "system", "content": Flowchart.CMPSC_Electives},
-        {"role": "system", "content": explain_credit_values}, # Explain credit values
-        {"role": "system", "content": Flowchart.four_credit},
-        {"role": "system", "content": explain_courses_taken}, # Explain courses taken
-        {"role": "system", "content": taken_courses},
-        {"role": "system", "content": explain_major_credits}, # Explain major credits
-        {"role": "system", "content": Flowchart.major_cred},
-        {"role": "system", "content": create}, # Create the schedule
+
+        {"role": "system", "content": prompt_one},
+        {"role": "system", "content": prompt_two},
+        {"role": "system", "content": prompt_three}
+
     ],
     model = "gpt-3.5-turbo"
 )
 
-print(chat_completion.choices[0].message.content)
+schedule = chat_completion.choices[0].message.content
+
+
+schedule_dict = json.loads(schedule)
+print(schedule_dict)
