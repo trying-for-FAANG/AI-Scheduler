b6c75b700ded68d4c0b21ab53f41b2d4dd85dca6
Eric1K <ericfeng26@gmail.com>
2024-03-16 21:27:18 -0400

    part 2

--- flaskmain/routes.py
+++ flaskmain/routes.py
@@ -1,7 +1,23 @@
 from flask import Flask, render_template, url_for, flash, redirect, request
 from flaskmain import application
+from flaskmain.forms import SubmitForm
 
-@application.route("/")
-@application.route("/home")
+@application.route("/", methods = ["GET", "POST"])
+@application.route("/home", methods = ["GET", "POST"])
 def home():
-    return render_template("index.html", title = "Home")
+    form = SubmitForm()
+    if form.validate_on_submit():
+        print("okay")
+    if request.method == 'POST':
+        selected_major = request.form["major"]
+        semester = request.form["semester"]
+        message = request.form["message"]
+        # Process the selected major as needed
+        print(selected_major)
+        list = []
+        list.append(selected_major)
+        list.append(semester)
+        list.append(message)
+        return f'Selected major: {list}'
+    
+    return render_template("index.html", title = "Home", form = form)
