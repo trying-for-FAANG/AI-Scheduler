6d45d30f055fc4136555f268329da5119bb9699d
LtPlaz <75555968+LtPlaz@users.noreply.github.com>
2024-03-16 22:57:33 -0400

    Update GPT_Integration.py
    cool

--- GPT_Integration.py
+++ GPT_Integration.py
@@ -8,24 +8,25 @@ taken_courses = 'hi'
 preferences = 'None'
 introduction = "You are a Penn State Engineering Advisor, specifically for Computer Science. Your goal is to create an academic schedule based off the given content."
 explain_main_courses = "Here is a dictionary with all the keys as required courses to take as a Computer Science Major. \
-           The format for the value is that the first element can be an equilavent course you can take if there is a dash (\"-\") in front of it. \
-           Then if the last element of the list in the value is an integer, the integer represents the minimum semester the user must be in to take that specific course.\
-           Additionally, if the characters -c are attached to the end of a class, this signifies that the course is a concurrent requirement for the key course.\
-           Every other element is a pre-requisite (prereq) courses. "
+           An element can be a equivalent course you can take if there is a dash (-) in front of it. \
+           If the last element of the list in the value is an integer, the integer represents the minimum semester status the user must be in to take that specific course.\
+           If the characters -c are attached to the end of a course, this signifies that the course is a concurrent requirement for the key course.\
+           Every other element is a pre-requisite (prereq) course. Every course (key) within this dictionary should be considered a major course."
 explain_general_education = "Here is a dictionary with all of the general education credits. \
            Each key refers to a category of general education, this can be treated as a course. \
-           Each key's value refers to the credits needed for that specific course.\
+           Each key's value refers to the total credits that are needed for that specific course.\
            General education credit courses can be displayed as their key values (type of general education credit - not actual specific course) when put into a schedule.\
-           Within a single semester in the schedule, aim to include 1-2 general education courses to spread it out."
+           Within a single semester in the schedule, aim to include 1-2 general education courses."
 explain_CMPSC_electives = f"Within the dictionary of all the general education credits, there is a key that is labeled \"CMPSC Electives\"\
-        There is an additional dictionary named CMPSC_Electives. Based on the user preferenced provided in {preferences}, choose 6 credits worth of cources. All the courses are 3 credits " 
+           Here is an additional dictionary named CMPSC_Electives. Based on the user's preferences being: {preferences}, choose 6 credits worth of courses."
 explain_credit_values = "Here is a list of all the courses with a credit value of 4.\
            CMPSC 111 is 1 credit. If a course is not within this list and is not CMPSC 111, it can be assumed that the course is worth 3 credits."
-explain_courses_taken = "Here is a lisT of all of the credits that the student has already taken. \
+explain_courses_taken = "Here is a list of all of the credits that the student has already taken. \
             Add all of these credits to the total credits needed for the student's degree"
-explain_major_credits = "This number is the total number of credits from courses that are not considered general education credits needed for a student to obtain their Computer Science degree."
+explain_major_credits = "This number is the total required major credits, that is, the total number of credits from courses that are not considered general education credits needed for a student to obtain their Computer Science degree."
 create = "Create a schedule for the student's remaining semesters that satisfies the major credits that are still needed and the general education credits that are still needed.\
-           Attempt to satisfy the student's CMPSC_electives courses preferences but do not prioritize those preferences. Format the schedule as a dictionary "
+           Attempt to satisfy the student's CMPSC_electives courses preferences but do not prioritize those preferences.\
+           Try to keep the total credits that a student takes within a semester between 15 and 17. Format the schedule as a dictionary "
 
 
 chat_completion = client.chat.completions.create(
@@ -48,5 +49,5 @@ chat_completion = client.chat.completions.create(
     model = "gpt-3.5-turbo"
 )
 
-#print(chat_completion.choices[0].message.content)
-print(Flowchart.cmpsc_courses)
+schedule = chat_completion.choices[0].message.content
+
