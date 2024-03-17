8e03cc51268019942c8677f64bd23b3d798936fa
esinocchi <163606662+esinocchi@users.noreply.github.com>
2024-03-17 04:10:27 -0400

    Update GPT_Integration.py

--- GPT_Integration.py
+++ GPT_Integration.py
@@ -10,48 +10,75 @@ client = OpenAI(api_key = OPENAI_API_KEY)
 
 
 semester = '0'
-taken_courses = 'CMPSC 131'
+taken_courses = ''
+taken_courses = taken_courses.split(',')
 preferences = 'None'
 courses_str = json.dumps(Flowchart.cmpsc_courses, indent=2)
 gen_ed_str = json.dumps(Flowchart.general_education, indent=2)
 cmpsc_electives_str = json.dumps(Flowchart.CMPSC_Electives, indent=2)
 four_credit_str = json.dumps(Flowchart.four_credit, indent=4)
 priority_str = json.dumps(Flowchart.priority, indent=4)
+difficulties = "1 for easy, 2 for medium, and 3 for hard"
 
-prompt_one = f'You are a Penn State Engineering Advisor, specifically for Computer Science. Your goal is to create an academic schedule that meets graduation requirenets based off the given content. \
-Attached is a dictonary: {courses_str} , with the keys as required courses needed to graduate. Lets call the key, key course. The values is a dictionary. \
-In that dictionary, there are concurrent courses which can be taken with the key course, then pre-requisite courses that must be taken BEFORE the key course. \
-Another dictionary: {priority_str} , is a list of courses that you are going to prioritize to take as soon as possible.'
 
-prompt_two = f'In addition, there is a dictionary: {gen_ed_str} , with all of the general education credits. Each key is treated as a course and the value refers to the total credits needed to fulfill the general education requirement. Add enough general educuation courses to fulfill its specific credit requirement. \
-In the general education dictionary, there is a key named CMPSC Electives. This is a special case. There is another dictionary named preferences: {preferences}. Based off the preferences, choose courses in the CMPSPC Electives dictionary: {cmpsc_electives_str}. Take enough credits of the classes provided to fulfill the cmpsc electives general education requirement.\
-NOTE: The CMPSC electives are DIFFERENT than the department (engineering) electives. For the credit values, here is a list: {four_credit_str}, with all courses that have 4 credits. CMPSC 111 has 1 credit. Assume all other courses are 3 credits. \
-Given that the student has already taken: {semester}, in the schedule create the remaining semesters so the total semesters adds up to 8. For example, if someone has already taken 3 semesters, create 5.'
 
-prompt_three = f'This string: {taken_courses}, has all the courses already taken. Do not add these courses in the string to the schedule. These also fulfill the prequisite requirements. For example, if you have already taken MATH 140, you can take MATH 141. \
-If the user has taken 0 semesters, these are the UNIQUE requirements for the first two semesters: must take CMPSC 111, ENG 015, CMPSC 131 and most of the other classes in priority dictionary mentioned before \
-These are the requirements for each semesters, including the first two: 15-19 credits, 1-2 general education courses \
-These are the requirements for the entire schedule: 8 semesters max, 45 general education credits, 82 major credits\
-Output the schedule in the form of a dictionary. Do not include credits in the schedule. Include type of general education credit. A sample output looks like: \
-    Semester 1: \
-        CMPSC 131 \
-        MATH 140 \
-        ENGL 015 \
-        GN General Education \
-    ... \
-    Semester 7: \
-        CMPSC 450 \
-        CMPSC 483W or CMPSC 431W \
-        Department (Engineering) Elective \
-    In addition, \'Department (Engineering) Elective\' is added, treat it as a 3 credit course. Therefore you will need to add it three times. Implement this into a dictionary.'
+prompt = f"""\
+#HACKPSU AI Schedule Advisor\n\
+\n\
+The goal is to create an academic schedule for a computer science student at Penn State University, taking into account the {preferences}. \
+Here's a summary of the data structures available for creating the schedule:\n\
+\n\
+- **CMPSC Courses**: {courses_str}\n\
+This dictionary contains computer science courses along with their concurrent courses, prerequisites, and a difficulty level.\n\
+\n\
+- **Taken Courses**: {taken_courses}\n\
+A list of courses that have already been taken\n\
+- **Semesters**: {semester}\n\
+The number of semesters already taken.
+- **Four Credit Courses**: {four_credit_str}\n\
+A list of courses that are worth 4 credits each.\n\
+\n\
+- **Priority Courses**: {priority_str}\n\
+A list of courses that should be prioritized in the student's schedule.\n\
+\n\
+- **General Education Requirements**: {gen_ed_str}\n\
+A dictionary outlining the credits required for general education categories.\n\
+\n\
+-**Preferences**: {preferences}\n\
+Prefences that should be considered when choosing electives or additional courses.\n\
+- **CMPSC elective courses**: {cmpsc_electives_str}\n\
+A detailed list of CMPSC elective courses including descriptions and prerequisites. The courses chosen should align with the preferences.'\n\
+Given the student's preferences and the need to fulfill the CMPSC elective requirement, select electives that complement the core curriculum and enhance the student's skills in areas of interest such as software engineering, data science, or cybersecurity. For example, if focusing on cybersecurity, include 'CMPSC 443: Introduction to Computer and Network Security' among the elective courses.\n\
+\n\
+Given the constraints of 45 general education credits and 82 major credits to graduate, create a balanced academic schedule that:\n\
+- Do not add courses that have already been taken in the Taken Courses list\n\
+- Ensures that prerequisites and concurrent courses are satisfied.\n\
+- Incorporates priority courses early in the academic career.\n\
+- Fulfills both general education and major requirements.\n\
+- If the course in not General Education, include the specific course\n\
+- Considers the difficulty of courses to balance the workload each semester, aiming for a mix that manages the student's stress and academic success.\n\
+- Aims for a total of 8 semesters, with previous semesters taken into account. Each semester including 4-6 courses, balancing between 1-2 general education courses and major courses.\n\
+\n\
+Please format the schedule as a dictionary, with each key representing a semester and its value being a list of courses to be taken that semester.\n\
+The next prompt is a sample output. Do not copy the courses course by course.\
+"""
+
+schedule_sample_output = "{\n\
+    \"Semester X\": [\n\
+        \"COURSE XXX\",\n\
+        \"COURSE XXX\",\n\
+        \"COURSE XXX\",\n\
+        \"COURSE XXX\"\n\
+    ],\n\
+    }"
+
 
 
 chat_completion = client.chat.completions.create(
     messages=[
 
-        {"role": "system", "content": prompt_one},
-        {"role": "system", "content": prompt_two},
-        {"role": "system", "content": prompt_three}
+        {"role": "system", "content": prompt},
+        {"role": "system", "content": schedule_sample_output}
 
     ],
     model = "gpt-3.5-turbo"
